1bb5d0615641b4f27062330765ecfb5597ac932b
Russell Morris <wrussellmorris@gmail.com>
2013-02-17 00:57:16 -0500

    Updating binder initialization

--- smf4j-api/src/main/java/org/smf4j/RegistrarFactory.java
+++ smf4j-api/src/main/java/org/smf4j/RegistrarFactory.java
@@ -15,6 +15,11 @@
  */
 package org.smf4j;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
@@ -33,8 +38,14 @@ public final class RegistrarFactory {
     private static final int NOT_INITIALIZED = 0;
     private static final int INITIALIZING = 1;
     private static final int NEED_RESOLUTION = 2;
-    private static final int SUCCESS = 3;
-    private static final int NOP = 4;
+    private static final int RESOLVING = 3;
+    private static final int SUCCESS = 4;
+    private static final int NOP = 5;
+
+    private static final String BINDER =
+            "org/smf4j/impl/StaticRegistrarBinder.class";
+    private static final String BINDER_FOR_UNIT_TESTS =
+            "org/smf4j/impl/StaticRegistrarBinderForUnitTests.class";
 
     private static final Logger log =
             LoggerFactory.getLogger("org.smf4j.RegistrarFactory");
@@ -42,11 +53,13 @@ public final class RegistrarFactory {
             new AtomicReference<RegistrarProvider>();
     private static final AtomicReference<RegistrarProvider> testProvider =
             new AtomicReference<RegistrarProvider>();
-    private static final Registrar nop = NopRegistrar.INSTANCE;
+    private static final Registrar NOP_REGISTRAR = NopRegistrar.INSTANCE;
     private static final AtomicInteger initState =
             new AtomicInteger(NOT_INITIALIZED);
     private static final AtomicBoolean foundBinding = new AtomicBoolean(false);
     private static final AtomicBoolean foundTestBinding =
+            new AtomicBoolean(false);
+    private static final AtomicBoolean foundDuplicates =
             new AtomicBoolean(false);
 
     private RegistrarFactory() {
@@ -55,9 +68,8 @@ public final class RegistrarFactory {
     public static Registrar getRegistrar() {
         initialize();
         switch(initState.get()) {
-            case INITIALIZING:
             case NOP:
-                return nop;
+                return NOP_REGISTRAR;
             case SUCCESS:
                 if(foundTestBinding.get()) {
                     return testProvider.get().getRegistrar();
@@ -65,7 +77,8 @@ public final class RegistrarFactory {
                     return provider.get().getRegistrar();
                 }
             default:
-                return nop;
+                // We shouldn't get here...
+                return NOP_REGISTRAR;
         }
     }
 
@@ -87,6 +100,7 @@ public final class RegistrarFactory {
             switch(curState) {
                 case NOT_INITIALIZED:
                     if(!initState.compareAndSet(curState, INITIALIZING)) {
+                        // Someone else beat us - let's take another pass
                         break;
                     }
 
@@ -96,12 +110,27 @@ public final class RegistrarFactory {
                         initState.set(NOP);
                         // Error: We could not find either provider.  We'll
                         // emit a nastygram and fall back to a NOP provider.
-                        nastygram();
+                        log.error("Using No-Operation (nop) Registrar "
+                                + "because neither a normal nor a test "
+                                + "RegistrarProvider could be found.");
+                        return;
+                    }
+                    if(foundDuplicates.get()) {
+                        initState.set(NOP);
+                        // Error: We found duplicate providers
+                        log.error("Using No-Operation (nop) Registrar since "
+                                + "duplicate binders were found.  See prior "
+                                + "log entries for details.");
                         return;
                     }
                     initState.set(NEED_RESOLUTION);
                     break;
+
                 case NEED_RESOLUTION:
+                    if(!initState.compareAndSet(NEED_RESOLUTION, RESOLVING)) {
+                        // Someone else beat us - let's take another pass
+                        break;
+                    }
                     if(foundTestBinding.get()) {
                         initTestBinder();
                     }
@@ -109,8 +138,17 @@ public final class RegistrarFactory {
                         initBinder();
                     }
                     initState.set(SUCCESS);
+                    return;
+
+                case INITIALIZING:
+                case RESOLVING:
+                    // Somebody else is actively initializing stuff, let's wait
+                    // for them to finish.
                     break;
-                default:
+
+                case SUCCESS:
+                case NOP:
+                    // Initializing is done, for better or worse
                     return;
             }
         }
@@ -118,10 +156,16 @@ public final class RegistrarFactory {
 
     private static void attemptTestBinder() {
         try {
+            if(detectDuplicateBinders(BINDER_FOR_UNIT_TESTS)) {
+               // There are duplicate test binders
+               foundDuplicates.set(true);
+            }
+
             StaticRegistrarBinderForUnitTests.getSingleton();
             foundTestBinding.set(true);
         } catch(NoClassDefFoundError e) {
             // Could not find a testing binding
+            log.debug("Did not find a StaticRegistrarBinderForUnitTests.");
             foundTestBinding.set(false);
         }
     }
@@ -130,17 +174,25 @@ public final class RegistrarFactory {
         StaticRegistrarBinderForUnitTests binding =
                 StaticRegistrarBinderForUnitTests.getSingleton();
         if(binding == null) {
-            // TODO: Something's messed up...
+            log.error("StaticRegistrarBinderForUnitTests.getSingleton() "
+                    + "returned null.");
             return;
         }
 
         Object obj = binding.getRegistrarProvider();
         if(obj == null) {
-            // TODO: Something's messed up...
+            log.error(
+                    "StaticRegistrarBinderForUnitTests.getRegistrarProvider() "
+                    + "returned null.");
             return;
         }
         if(!(obj instanceof RegistrarProvider)) {
-           // TODO: Something's messed up...
+            log.error(
+                    "StaticRegistrarBinderForUnitTests.getRegistrarProvider() "
+                    + "returned an object that was not an instance of "
+                    + "org.smf4j.spi.RegistrarProvider.  The returned value "
+                    + "was an instance of '"
+                    + obj.getClass().getCanonicalName() + "'");
             return;
         }
 
@@ -149,6 +201,11 @@ public final class RegistrarFactory {
 
     private static void attemptBinder() {
         try {
+            if(detectDuplicateBinders(BINDER)) {
+                // There are duplicate binders
+                foundDuplicates.set(true);
+            }
+
             StaticRegistrarBinder.getSingleton();
             foundBinding.set(true);
         } catch(NoClassDefFoundError e) {
@@ -160,23 +217,61 @@ public final class RegistrarFactory {
     private static void initBinder() {
         StaticRegistrarBinder binding = StaticRegistrarBinder.getSingleton();
         if(binding == null) {
-            // TODO: Something's messed up...
+            log.error("StaticRegistrarBinder.getSingleton() returned null.");
             return;
         }
 
         Object obj = binding.getRegistrarProvider();
         if(obj == null) {
-            // TODO: Something's messed up...
+            log.error(
+                    "StaticRegistrarBinder.getRegistrarProvider() "
+                    + "returned null.");
             return;
         }
         if(!(obj instanceof RegistrarProvider)) {
-           // TODO: Something's messed up...
+            log.error(
+                    "StaticRegistrarBinder.getRegistrarProvider() returned an "
+                    + "object that was not an instance of "
+                    + "org.smf4j.spi.RegistrarProvider.  The returned value "
+                    + "was an instance of '"
+                    + obj.getClass().getCanonicalName() + "'");
+            return;
         }
 
         provider.set((RegistrarProvider)obj);
     }
 
-    private static void nastygram() {
-        log.error("Unable to bind to a normal or test RegistrarProvider.");
+    private static boolean detectDuplicateBinders(String fqcn) {
+        Set<URL> binders = findMatchingBinders(fqcn);
+        if(binders.size() > 1) {
+            log.error("Found duplicate registrar binders:");
+            for(URL url : binders) {
+                log.error(url.toString());
+            }
+            return true;
+        }
+        return false;
     }
+
+    private static Set<URL> findMatchingBinders(String fqcn) {
+        Set<URL> matchingBinders = new HashSet<URL>();
+        ClassLoader classLoader = RegistrarFactory.class.getClassLoader();
+        Enumeration<URL> matches = null;
+        try {
+            if(classLoader != null) {
+                matches = classLoader.getResources(fqcn);
+            } else {
+                matches = ClassLoader.getSystemResources(fqcn);
+            }
+        } catch (IOException e) {
+            log.error("Erroring getting resources from classpath.", e);
+        }
+
+        while(matches != null && matches.hasMoreElements()) {
+            matchingBinders.add(matches.nextElement());
+        }
+
+        return matchingBinders;
+    }
+
 }
